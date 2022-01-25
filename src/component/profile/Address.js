e0afd528b77032e7e6fc9b6f7f45d79081671b4d
kais <bahrikais62@gmail.com>
2022-01-25 17:15:38 +0100

    first commit

---
+++ src/component/profile/Address.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Address = () => {
+  return <div>
+    <h2>sousse hammam sousse</h2>
+  </div>;
+};
+
+export default Address;
---
+++ src/component/profile/ProfilePhoto.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import image from './KAIS.IMG.jpg';
+
+const ProfilePhoto = () => {
+  return <div>
+    <img src={image} alt='image'width={200}/>
+  </div>;
+};
+
+export default ProfilePhoto;
